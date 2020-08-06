a030f0bb0ab919c892ae777eb29ef955545e716c
Saamahn Mahjouri <mahjouri.saamahn@gmail.com>
2020-08-06 11:25:50 -0500

    first commit

--- get_data.sh
+++ get_data.sh
@@ -2,8 +2,8 @@
 
 cd datasets/voc
 
-wget http://host.robots.ox.ac.uk/pascal/VOC/voc2007/VOCtrainval_06-Nov-2007.tar
-wget http://host.robots.ox.ac.uk/pascal/VOC/voc2012/VOCtrainval_11-May-2012.tar
+wget http://pjreddie.com/media/files/VOCtrainval_06-Nov-2007.tar
+wget http://pjreddie.com/media/file/VOCtrainval_11-May-2012.tar
 
 tar -xvf VOCtrainval_06-Nov-2007.tar
 tar -xvf VOCtrainval_11-May-2012.tar
